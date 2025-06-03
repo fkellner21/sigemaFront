5c9afe1d67a264b158affeedcfdf31811521fa41
cristianvelozzz <cr.velozz@gmail.com>
2025-06-03 17:24:32 -0300

    Comienzo de maquinas

--- src/app/components/maquina/tab/tab.component.ts
+++ src/app/components/maquina/tab/tab.component.ts
@@ -7,28 +7,43 @@ import { Marca } from '../../../models/marca';
 import { TipoEquipo } from '../../../models/tipoEquipo';
 import { marcaService } from '../../../services/marca.service';
 import { tipoEquipoService } from '../../../services/tipoEquipo.service';
+import { MaquinaService } from '../../../services/equipo.service';
+import { MaquinaComponent } from './listado/maquina.component';
+
 import { forkJoin } from 'rxjs';
+import { Equipo } from '../../../models/equipo';
 
 @Component({
   selector: 'tabEquipos',
   templateUrl: 'tab.component.html',
-  imports: [MatTabsModule, TipoEquipoComponent, MarcaComponent, ModeloComponent],
+  imports: [MatTabsModule, TipoEquipoComponent, MarcaComponent, ModeloComponent, MaquinaComponent],
+
 })
 export class TabEquipos implements OnInit{
 
   marcas: Marca[] = [];
   tipos: TipoEquipo[] = [];
-  constructor(private marcaService: marcaService, private tipoService: tipoEquipoService) {}
+  maquinas: Equipo[] = [];
 
-  ngOnInit(): void {
+ constructor(
+  private marcaService: marcaService,
+  private tipoService: tipoEquipoService,
+  private maquinaService: MaquinaService
+) {}
+
+
+ngOnInit(): void {
   forkJoin({
     marcas: this.marcaService.findAll(),
-    tipos: this.tipoService.findAll()
-  }).subscribe(({ marcas, tipos }) => {
+    tipos: this.tipoService.findAll(),
+    maquinas: this.maquinaService.findAll()
+  }).subscribe(({ marcas, tipos, maquinas }) => {
     this.marcas = marcas;
     this.tipos = tipos;
+    this.maquinas = maquinas;
   });
-  }
+}
+
   loadTipos() {
     this.tipoService.findAll().subscribe(data => {
     this.tipos = [...data];  //genera una copia de los datos para que note el cambio
@@ -36,5 +51,10 @@ export class TabEquipos implements OnInit{
   }
   loadMarcas() {
     this.marcaService.findAll().subscribe(data => {this.marcas = [...data]});
+  }
+
+  
+  loadMaquinas() {
+    this.maquinaService.findAll().subscribe(data => this.maquinas = [...data]);
   }
 }
