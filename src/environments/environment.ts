46f8aa0352d89a25c1b827e542f11b68b6070a74
Francisco Kellner <kellner.francisco@gmail.com>
2025-06-03 21:16:51 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 //  production: true,
-apiUrl:'http://localhost:8080'
-  //apiUrl: 'https://sigemabe-d0gke3fdbnfza9et.canadacentral-01.azurewebsites.net'
+//apiUrl:'http://localhost:8080'
+  apiUrl: 'https://sigemabe-d0gke3fdbnfza9et.canadacentral-01.azurewebsites.net'
 };
