c54dd94661c6cb02720b4d54132ac307d83668ff
cristianvelozzz <cr.velozz@gmail.com>
2025-06-03 22:33:09 -0300

    Cambios en maquina

--- src/app/models/equipo.ts
+++ src/app/models/equipo.ts
@@ -1,12 +1,13 @@
 import { EstadoEquipo } from "./enum/EstadoEquipo";
 import { modeloEquipo } from "./modeloEquipo";
+import { Posicion } from "./posicion";
 
 export class Equipo{
     id!:number;
     matricula!:string;
     observaciones!:string;
     cantidadUnidadMedida!:number;
-    idUltimaPosicion!:number;//todo cambiar por el objeto
+    ultimaPosicion!:Posicion;//todo cambiar por el objeto
     modeloEquipo!:modeloEquipo;
     estadoEquipo!:EstadoEquipo;
 }
