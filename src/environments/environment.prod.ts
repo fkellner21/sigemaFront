0cdc962b35d64050c98551ee979a675abee11438
joacoseo1 <82622845+joacoseo1@users.noreply.github.com>
2025-07-25 19:23:56 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //apiUrl:'http://localhost:8080'
-  apiUrl: 'https://sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
+  apiUrl: 'sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
 };
