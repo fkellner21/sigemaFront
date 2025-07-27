1881f6da89f2bdfae0109d417801d2e944b3ce4b
Francisco Kellner <kellner.francisco@gmail.com>
2025-07-27 16:09:53 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 //apiUrl:'http://localhost:8080'
  // apiUrl: 'https://sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
-    apiUrl:'http://3.225.94.35:8585'
+    apiUrl:'https://sigemaapi.online'
 };
