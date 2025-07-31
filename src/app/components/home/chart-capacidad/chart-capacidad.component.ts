e3ccccd0a95fecd7aad96ef2daa7245254e2a151
fkellner21 <kellner.francisco@gmail.com>
2025-07-31 09:29:51 -0300

    se crearon charts segun estado

---
+++ src/app/components/home/chart-capacidad/chart-capacidad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import ApexCharts from 'apexcharts'
+
+@Component({
+  selector: 'chart-capacidad',
+  imports: [],
+  templateUrl: './chart-capacidad.component.html',
+  styleUrl: './chart-capacidad.component.css'
+})
+export class ChartCapacidadComponent {
+
+}
