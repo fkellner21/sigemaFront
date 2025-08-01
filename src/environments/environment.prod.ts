5b997792832bd327c45cbe40469d6d76ac2b4b5a
fkellner21 <kellner.francisco@gmail.com>
2025-08-01 12:10:12 -0300

    cambio en url de azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
 //apiUrl:'http://localhost:8080'
-  apiUrl: 'https://sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
+  //apiUrl: 'https://sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
+  apiUrl: 'https://sigemabe-d0gke3fdbnfza9et.canadacentral-01.azurewebsites.net'
   //  apiUrl:'https://sigemaapi.online'
 };
